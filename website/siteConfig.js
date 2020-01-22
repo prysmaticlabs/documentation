b46d6072f041c10f995e510e31beddd7c58a0f9d
Celeste Ariana Seberras <38491560+har00ga@users.noreply.github.com>
2020-01-22 21:13:27 +0000

    Added API url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: "Prysm 'Sapphire' Testnet", // Title for your website.
   tagline: 'An Ethereum 2.0 client written entirely in Go.',
-  url: 'https://hakr.gg/', // Your website URL
-  baseUrl: '/prystage/', // Base URL for your project */
+  url: 'https://docs.prylabs.network', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
@@ -42,6 +42,7 @@ const siteConfig = {
     { href: 'https://prylabs.net', label: 'Activate a Validator' },
     { href: 'https://github.com/prysmaticlabs/prysm', label: 'GitHub' },
     { href: 'https://medium.com/prysmatic-labs', label: 'Blog' },
+    { href: 'https://api.prylabs.network', label: 'API' },
   ],
 
   // If you have users set above, you add it here:
