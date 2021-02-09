e3b67e7d1fb0f9682f3631ac7e7e6fddff8b509e
rauljordan <raul@prysmaticlabs.com>
2021-02-08 21:12:36 -0600

    Fix Logo Href

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -49,7 +49,7 @@ module.exports = {
         footer: {
             logo: {
                 alt: "Prysm Eth2 Docs",
-                href: '/',
+                href: '/docs',
                 src: 'img/Prysm.svg',
             },
             copyright: `Copyright © ${new Date().getFullYear()} Prysmatic Labs, LLC., Validator Deposit Contract 0x00000000219ab540356cbb839cbe05303d7705fa`,
