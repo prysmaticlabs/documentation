987b9150e7ac600e27a89d7ff2a9d05f55e33df7
rauljordan <raul@prysmaticlabs.com>
2021-09-28 14:11:58 -0400

    cache time

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -85,7 +85,6 @@ module.exports = {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
                 sitemap: {
-                    cacheTime: 600 * 1000,
                     changefreq: 'weekly',
                     priority: 0.5,
                 },
