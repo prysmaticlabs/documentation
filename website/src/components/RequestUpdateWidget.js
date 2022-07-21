cd952083c0387c6cea5b587e6c1783dc47379c98
symbolpunk <symbolpunk@gmail.com>
2022-07-21 13:09:27 -0400

    s

--- website/src/components/RequestUpdateWidget.js
+++ website/src/components/RequestUpdateWidget.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const RequestUpdateWidget = ({ docTitle }) => (
 	<div class="update-request">
-		<a href={`https://github.com/prysmaticlabs/documentation/issues/new?title=Docs update request: ${docTitle}`}>🐼 Request an update</a>
+		<a href={`https://github.com/prysmaticlabs/documentation/issues/new?title=Docs update request: ${docTitle}&body=Source:${window.location.href}`}>🐼 Request an update</a>
 	</div>
 ); 654
