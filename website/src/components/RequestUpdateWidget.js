0aa908fdca7f98abc10371e51dfd5a4e6a3baf47
symbolpunk <symbolpunk@gmail.com>
2022-07-21 11:22:50 -0400

    s

--- website/src/components/RequestUpdateWidget.js
+++ website/src/components/RequestUpdateWidget.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const RequestUpdateWidget = ({ docTitleToUse }) => (
 	<div class="update-request">
-		<a href="https://github.com/prysmaticlabs/documentation/issues/new?title=Content%20Update%20Request:%20Getting%20Started">🐼 Request an update - {docTitleToUse}</a>
+		<a href={`https://github.com/prysmaticlabs/documentation/issues/new?title=${docTitleToUse}`}>🐼 Request an update</a>
 	</div>
 );
