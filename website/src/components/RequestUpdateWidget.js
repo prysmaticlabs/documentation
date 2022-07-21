993029af07568096bf538ba426cf6e0b31c7ff9d
symbolpunk <symbolpunk@gmail.com>
2022-07-21 13:16:54 -0400

    s

--- website/src/components/RequestUpdateWidget.js
+++ website/src/components/RequestUpdateWidget.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
-export const RequestUpdateWidget = ({ docTitle }) => (
-	<div class="update-request">
-		<a href={`https://github.com/prysmaticlabs/documentation/issues/new?title=Docs update request: ${docTitle}&body=Source:${window.location.href}`}>🐼 Request an update</a>
-	</div>
+export const RequestUpdateWidget = ({ docTitle, currentUrl }) => (
+	<BrowserOnly>
+		{() =>
+			<div class="update-request">
+				<a href={`https://github.com/prysmaticlabs/documentation/issues/new?title=Docs update request: ${docTitle}&body=Source:${currentUrl}`}>🐼 Request an update</a>
+			</div>
+		}
+	</BrowserOnly>
 ); 654
