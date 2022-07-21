4a6320eed3889b5fa4ef416cbcd2e4ec204c4e7d
symbolpunk <symbolpunk@gmail.com>
2022-07-21 10:18:19 -0400

    s

--- website/src/components/RequestUpdateWidget.js
+++ website/src/components/RequestUpdateWidget.js
@@ -1,21 +1,5 @@
 import React from 'react';
 
-export default function RequestUpdateWidget({ children, color }) {
-	return (
-		<span
-			style={{
-				backgroundColor: color,
-				borderRadius: '2px',
-				color: '#fff',
-				padding: '0.2rem',
-			}}>
-			{children}
-		</span>
-	);
-}
-
-
-
 export const RequestUpdateWidget = ({ children, color }) => (
 	<span
 		style={{
