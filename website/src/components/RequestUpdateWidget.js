f254736134bf56eff36e11f1ff631c378aa843b0
symbolpunk <symbolpunk@gmail.com>
2022-07-21 12:02:44 -0400

    s

--- website/src/components/RequestUpdateWidget.js
+++ website/src/components/RequestUpdateWidget.js
@@ -4,4 +4,4 @@ export const RequestUpdateWidget = ({ docTitleToUse }) => (
 	<div class="update-request">
 		<a href={`https://github.com/prysmaticlabs/documentation/issues/new?title=${docTitleToUse}`}>🐼 Request an update</a>
 	</div>
-);
+); 654
