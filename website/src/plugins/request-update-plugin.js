f992ddb1854707ae95f7f7232cd14e69d7aab613
symbolpunk <symbolpunk@gmail.com>
2022-07-21 10:44:32 -0400

    s

--- website/src/plugins/request-update-plugin.js
+++ website/src/plugins/request-update-plugin.js
@@ -1,31 +1,9 @@
 module.exports = function myPlugin(context, options) {
     return {
         name: 'request-update-plugin',
-        async loadContent() {
-            return { remoteHeadTags: await fetchHeadTagsFromAPI() };
-        },
         injectHtmlTags({ content }) {
             return {
-                headTags: [
-                    {
-                        tagName: 'link',
-                        attributes: {
-                            rel: 'preconnect',
-                            href: 'https://www.github.com',
-                        },
-                    },
-                    ...content.remoteHeadTags,
-                ],
-                preBodyTags: [
-                    {
-                        tagName: 'script',
-                        attributes: {
-                            charset: 'utf-8',
-                            src: '/noflash.js',
-                        },
-                    },
-                ],
-                postBodyTags: [`<div> This is post body </div>`],
+                postBodyTags: [`<div> This is post body </div>`]
             };
         },
     };
