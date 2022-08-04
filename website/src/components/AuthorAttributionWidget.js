0d8e0370de875c6dbcbea724bef48c5a5b0fe126
symbolpunk <symbolpunk@gmail.com>
2022-08-04 14:22:42 -0400

    test

--- website/src/components/AuthorAttributionWidget.js
+++ website/src/components/AuthorAttributionWidget.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
-export const AuthorAttributionWidget = ((author) => {
+export const AuthorAttributionWidget = ({ author }) => {
 	let convertNameToGithubHandle = function (name) {
 		return "symbolpunk";
 	}
 
 	return (<a class="author-attribution" href={`https://github.com/${(convertNameToGithubHandle)}`}>{author}</a>)
-});
+};
 
