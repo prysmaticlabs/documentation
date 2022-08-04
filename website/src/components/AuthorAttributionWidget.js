b2e6ada3b00f95224335ef12ef1634b1d0feee25
symbolpunk <symbolpunk@gmail.com>
2022-08-04 14:46:12 -0400

    fix

--- website/src/components/AuthorAttributionWidget.js
+++ website/src/components/AuthorAttributionWidget.js
@@ -13,7 +13,7 @@ export const AuthorAttributionWidget = ({ authors }) => {
 	let buildAuthorBadge = function (author) {
 		return (
 			<a class="author-attribution" href={`https://github.com/${githubUsernames[author]}`}>
-				<span class="author-avatar" style={`"background-image: url('https://avatars.githubusercontent.com/${githubUsernames[author]}')"`}></span>
+				<span class="author-avatar" style={{ backgroundImage: url('https://avatars.githubusercontent.com/${githubUsernames[author]}') }}></span>
 				<span class="author-name">{author}</span>
 			</a>
 		)
