f0a2bb34e81a8da52aeeb32d386a4ef717f17f0a
symbolpunk <symbolpunk@gmail.com>
2022-08-04 16:12:57 -0400

    css

--- website/src/components/HeaderBadgesWidget.js
+++ website/src/components/HeaderBadgesWidget.js
@@ -11,8 +11,8 @@ export const HeaderBadgesWidget = ({ authors }) => {
 	let buildAuthorBadge = function (author) {
 		return (
 			<a class="header-badge" href={`https://github.com/${githubUsernames[author]}`}>
-				<span class="author-avatar" style={{ backgroundImage: "url('https://avatars.githubusercontent.com/" + githubUsernames[author] + "')" }}></span>
-				<span class="author-name">{author}</span>
+				<span class="badge-avatar" style={{ backgroundImage: "url('https://avatars.githubusercontent.com/" + githubUsernames[author] + "')" }}></span>
+				<span class="badge-label">{author}</span>
 			</a>
 		)
 	}
@@ -20,13 +20,11 @@ export const HeaderBadgesWidget = ({ authors }) => {
 	return (
 		<BrowserOnly>
 			{() =>
-				<div>
-					<div class="header-badges">{authors.split(',').map(buildAuthorBadge)}</div>
-					<a href={`https://github.com/prysmaticlabs/documentation/issues/new?title=Docs update request: ${new URL(window.location.href).pathname}&body=Source: ${window.location.href}%0A%0ARequest: (how can we help?)`}>🐼 Request an update</a>
-
+				<div class="header-badges">
+					{authors.split(',').map(buildAuthorBadge)}
+					<a class="header-badge" href={`https://github.com/prysmaticlabs/documentation/issues/new?title=Docs update request: ${new URL(window.location.href).pathname}&body=Source: ${window.location.href}%0A%0ARequest: (how can we help?)`}><span class="badge-label">Request an update</span></a>
 				</div>
 			}
 		</BrowserOnly>
 	);
 };
-
