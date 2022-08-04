d85b0a5920d75471d30aeb8e97613e1f4006c69c
symbolpunk <symbolpunk@gmail.com>
2022-08-04 14:42:05 -0400

    test

--- website/src/components/AuthorAttributionWidget.js
+++ website/src/components/AuthorAttributionWidget.js
@@ -2,12 +2,16 @@ import React from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export const AuthorAttributionWidget = ({ authors }) => {
-	let convertNameToGithubHandle = function (author) {
-		return "symbolpunk";
+	let githubUsernames = {
+		Mick: "symbolpunk"
+	}
+
+	let githubAvatars = {
+		Mick: "symbolpunk"
 	}
 
 	let buildAuthorBadge = function (author) {
-		return (<a class="author-attribution" href={`https://github.com/${(convertNameToGithubHandle(author))}`}><span>{author}</span></a>)
+		return (<a class="author-attribution" href={`https://github.com/${githubUsernames[author]}`}><span class="author-avatar" style={`background-image: url('https://avatars.githubusercontent.com/${githubUsernames[author]}')`}></span><span class="author-name">{author}</span></a>)
 	}
 
 	return <div>{authors.split(',').map(buildAuthorBadge)}</div>;
