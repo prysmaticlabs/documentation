29f670cdeb5875d0dcfd324c760ad402625b5263
symbolpunk <symbolpunk@gmail.com>
2022-08-04 16:21:44 -0400

    github icon

--- website/src/components/HeaderBadgesWidget.js
+++ website/src/components/HeaderBadgesWidget.js
@@ -23,7 +23,7 @@ export const HeaderBadgesWidget = ({ authors }) => {
 				<div class="header-badges">
 					{authors.split(',').map(buildAuthorBadge)}
 					<a class="header-badge" href={`https://github.com/prysmaticlabs/documentation/issues/new?title=Docs update request: ${new URL(window.location.href).pathname}&body=Source: ${window.location.href}%0A%0ARequest: (how can we help?)`}>
-						<span class="badge-avatar" style={{ backgroundImage: "url('https://i.imgur.com/B0bkhqa.png')" }}></span>
+						<span class="badge-avatar" style={{ backgroundImage: "url('https://avatars.githubusercontent.com/github')" }}></span>
 						<span class="badge-label">Request an update</span>
 					</a>
 				</div>
