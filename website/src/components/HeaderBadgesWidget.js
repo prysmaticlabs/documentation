adca4a3026b0243526c61e63e1f057eedfeb9bf0
symbolpunk <symbolpunk@gmail.com>
2022-08-04 17:05:21 -0400

    s

--- website/src/components/HeaderBadgesWidget.js
+++ website/src/components/HeaderBadgesWidget.js
@@ -27,7 +27,7 @@ export const HeaderBadgesWidget = ({ authors }) => {
 						<span class="badge-label emoji-avatar">Request an update</span>
 					</a>
 					<a class="header-badge" href={`https://github.com/prysmaticlabs/documentation/issues/new?title=Docs update request: ${new URL(window.location.href).pathname}&body=Source: ${window.location.href}%0A%0ARequest: (how can we help?)`}>
-						<span class="badge-avatar">✔️</span>
+						<span class="badge-avatar emoji-avatar">✔️</span>
 						<span class="badge-label">Last verified on August 4th using Prysm v2.1.4-rc0</span>
 					</a>
 				</div>
