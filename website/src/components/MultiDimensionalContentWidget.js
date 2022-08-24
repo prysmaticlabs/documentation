aa254978f1d79579a3f278f12a0cb3fbf6698b4d
symbolpunk <symbolpunk@gmail.com>
2022-08-24 12:10:35 -0400

    s

--- website/src/components/MultiDimensionalContentWidget.js
+++ website/src/components/MultiDimensionalContentWidget.js
@@ -2,21 +2,16 @@ import React from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export const MultiDimensionalContentWidget = () => {
+
+	let bindTabs = function () {
+		alert('heyo 1');
+		var tabElements = document.querySelectorAll('.tabs__item');
+		tabElements.forEach(element => element.addEventListener("click", function () { alert('heyo'); }));
+	}
+
 	return (
 		<BrowserOnly>
-			{() =>
-				<script>
-					{`
-					alert('heyo 1');
-					function(){
-						alert('heyo 2');
-						var tabElements = document.querySelectorAll('.tabs__item');
-						foreach(var el in tabElements){
-							el.addEventListener("click", function () { alert('heyo'); })
-					}}();
-					`}
-				</script>
-			}
+			{() => { bindTabs() }}
 		</BrowserOnly>
 	);
 };
