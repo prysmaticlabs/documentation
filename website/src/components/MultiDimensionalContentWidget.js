7e7743c0394f13235b36cce22d683a86cd82240d
symbolpunk <symbolpunk@gmail.com>
2022-08-24 11:44:59 -0400

    s

--- website/src/components/MultiDimensionalContentWidget.js
+++ website/src/components/MultiDimensionalContentWidget.js
@@ -6,10 +6,12 @@ export const MultiDimensionalContentWidget = () => {
 		<BrowserOnly>
 			{() =>
 				<script>
+					{`
 					var tabElements = document.querySelectorAll('.tabs__item');
 					foreach(var el in tabElements){
 						el.addEventListener("click", function () { alert('heyo'); })
 					}
+					`}
 				</script>
 			}
 		</BrowserOnly>
