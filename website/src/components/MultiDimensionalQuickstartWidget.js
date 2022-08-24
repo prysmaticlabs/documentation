d254e3e7f8704b2b77d66994fdc76ee533d34f07
symbolpunk <symbolpunk@gmail.com>
2022-08-24 13:30:59 -0400

    s

--- website/src/components/MultiDimensionalQuickstartWidget.js
+++ website/src/components/MultiDimensionalQuickstartWidget.js
@@ -6,25 +6,28 @@ export const MultiDimensionalQuickstartWidget = () => {
 	let bindTabs = function () {
 		setTimeout(function () {
 			var tabElements = document.querySelectorAll('.quickstart-tabs .tabs__item');
-			tabElements.forEach(element => element.addEventListener("click", function (event) {
-				var targetElement = event.target;
-				var textContent = targetElement.textContent;
-				var isLabel = textContent.indexOf(":") > -1;
+			tabElements.forEach(element => {
+				var isLabel = element.textContent.indexOf(":") > -1;
 				if (isLabel) {
 					console.log('isLabel2')
-					// unbind
-					targetElement.parentElement.innerHTML = targetElement.parentElement.innerHTML;
+					// unbind so tab can't be selected
+					element.parentElement.innerHTML = element.parentElement.innerHTML;
 				} else {
-					if (textContent == 'Besu') {
-						// disable IPC, select http
-						alert(textContent);
-					} else if (textContent == 'Geth' || textContent == 'Nethermind') {
-						// enable IPC
-					} else if (textContent == 'IPC') {
-						// disable Besu
-					}
+					element.addEventListener("click", function (event) {
+						var targetElement = event.target;
+						var textContent = targetElement.textContent;
+
+						if (textContent == 'Besu') {
+							// disable IPC, select http
+							alert(textContent);
+						} else if (textContent == 'Geth' || textContent == 'Nethermind') {
+							// enable IPC
+						} else if (textContent == 'IPC') {
+							// disable Besu
+						}
+					}, false)
 				}
-			}, false));
+			});
 			console.log("start with the user, work backwards to the technology...");
 			console.log("start with utopia, work backwards to the mechanism design...");
 		}, 500)
