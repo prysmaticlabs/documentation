eb60806dcabadd05a8b41308eae9e3936c97804c
symbolpunk <symbolpunk@gmail.com>
2022-08-24 11:31:41 -0400

    s

--- website/src/components/MultiDimensionalContentWidget.js
+++ website/src/components/MultiDimensionalContentWidget.js
@@ -2,5 +2,13 @@ import React from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export const MultiDimensionalContentWidget = () => {
-	document.getElementsByClassName('tabs__item').onclick = function () { alert('heyo') };
+	return (
+		<BrowserOnly>
+			{() =>
+				<script >
+					document.getElementsByClassName('tabs__item').onclick = function () {alert('heyo')};
+				</script>
+			}
+		</BrowserOnly>
+	);
 };
