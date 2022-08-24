0a66509b443d865853840240a7de29f212ae930d
symbolpunk <symbolpunk@gmail.com>
2022-08-24 12:24:22 -0400

    s

--- website/src/components/MultiDimensionalContentWidget.js
+++ website/src/components/MultiDimensionalContentWidget.js
@@ -6,7 +6,6 @@ export const MultiDimensionalContentWidget = () => {
 	let bindTabs = function () {
 		setTimeout(function () {
 			var tabElements = document.querySelectorAll('.quickstart-tabs .tabs__item');
-			console.log("tabs: " + tabElements.length);
 			tabElements.forEach(element => element.addEventListener("click", function () { alert('heyo'); }));
 			console.log("start with the user, work backwards to the technology...");
 			console.log("start with utopia, work backwards to the mechanism design...");
