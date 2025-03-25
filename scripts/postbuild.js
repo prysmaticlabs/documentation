1b7eb9a0cbb5e55d85f31ee64994a4d296bac6a5
Alec Thomas <112640918+a-thomas-22@users.noreply.github.com>
2025-03-25 13:30:12 -0500

    Update postbuild.js

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 // Paths
 const sourceFile = path.join(__dirname, '../build/prysm/docs/404.html');
-const targetFile = path.join(__dirname, '../build/404.html');
+const targetDir = path.join(__dirname, '../build/prysm/docs/404');
+const targetFile = path.join(targetDir, 'index.html');
 
 // Ensure target directory exists
-const targetDir = path.dirname(targetFile);
 if (!fs.existsSync(targetDir)) {
     fs.mkdirSync(targetDir, { recursive: true });
 }
@@ -14,7 +14,7 @@ if (!fs.existsSync(targetDir)) {
 // Copy the file
 try {
     fs.copyFileSync(sourceFile, targetFile);
-    console.log('Successfully copied 404.html to build root');
+    console.log('Successfully copied 404.html to /prysm/docs/404/index.html');
 } catch (err) {
     console.error('Error copying 404.html:', err);
     process.exit(1);
