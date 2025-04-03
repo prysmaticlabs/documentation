89fc1b773a0a22fd5af2743623b4cb6d9272702e
Alec Thomas <112640918+a-thomas-22@users.noreply.github.com>
2025-04-03 10:41:41 -0500

    config(meta): update default OpenGraph image and description
    - Set prysm_together.png as default image for social sharing
    - Add community-focused meta description for homepage
    - Move metadata configuration to root level per Docusaurus spec

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,6 @@ module.exports = {
     staticDirectories: ['static'],
 
     customFields: {
-        image: 'images/Prysm.svg',
         prysmVersion: prysmVersion,
     },
     trailingSlash: true,
@@ -66,9 +65,11 @@ module.exports = {
         prism: {
             theme: require('prism-react-renderer/themes/dracula'),
         },
-        ogImage: 'images/Prysm.svg',
-        twitterImage: 'images/Prysm.svg',
     },
+    metadata: [
+        { name: 'description', content: 'Join our community of innovators shaping the decentralized future. Your node matters.' }
+    ],
+    image: 'images/prysm_together.png',
     presets: [
         [
             '@docusaurus/preset-classic',
